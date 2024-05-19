c859cc3672c7023531c16328315c91a76e2677da
Awanish Verma <avanishverma4@gmail.com>
2024-05-19 12:28:47 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// let indicator = document.querySelector(".content-container");
+// let maxHeight = document.documentElement.scrollHeight - window.innerHeight;
+
+// window.addEventListener("scroll", () => {
+//   let scrollRatio = window.scrollY / maxHeight;
+//   let angle = scrollRatio * 360;
+//   indicator.style.background = `conic-gradient(#03a9f4 ${angle}deg, #e91e63 0deg)`;
+// });
+
 function toggleMenu() {
   const menu = document.querySelector(".menu-links");
   const icon = document.querySelector(".hamburger-icon");
